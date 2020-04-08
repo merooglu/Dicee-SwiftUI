857037920bdadd403a41d95a75232a43ab9b0c0e
Mehmet Eroğlu <mehmet---eroglu@hotmail.com>
2020-04-08 11:25:46 +0300

    using swiftUI functionality

---
+++ Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Mehmet Eroğlu on 8.04.2020.
+//  Copyright © 2020 Mehmet Eroğlu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(num: leftDiceNumber)
+                    DiceView(num: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color("diceColor"))
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    let num: Int
+    var body: some View {
+        Image("dice\(num)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
